41b6bd8f9cd340a3b0ac89d804cb5b171374840e
Naomi <naomi.ungureanu@inga-hameln.de>
2024-06-17 10:15:52 +0200

    some changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToDoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
