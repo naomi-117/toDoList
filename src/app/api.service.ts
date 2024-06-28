fa77c80068d56c6b712d4e261782a1d6b13ed655
Naomi <naomi.ungureanu@inga-hameln.de>
2024-06-28 10:47:38 +0200

    post works

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
-import { Observable, } from 'rxjs';
+import { Observable } from 'rxjs';
+import { Task } from './task';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ApiService {
-  private apiUrl = 'http://localhost:8080';
+  private apiUrl = 'http://localhost:8080/todolist'; // !!!
 
   constructor(private http: HttpClient) { }
 
   getData(description: string): Observable<any> {
-    console.warn('im here', description);
+    // console.warn('im here', description);
     return this.http.get<any>(`${this.apiUrl}/todolist/${description}`);
   }
  
-  postData(description: string): void {
-    console.warn('im here', description);
-    
-    this.http.post<any>(`${this.apiUrl}/todolist/${description}`, description).subscribe(data => {
+  postData(task: Task): void {    
+    this.http.post<any>(`${this.apiUrl}/todolist`, task).subscribe(data => {
       console.warn(data);
     });
   }
